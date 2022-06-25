9ddfb72ba9dbd3671d76ab9ac8ce63566c43a76e
Miroslav Taleiko <mtaleiko@gmail.com>
2022-06-25 10:38:06 +0300

    Initial Commit

---
+++ PrefKeyTests/PrefKeyTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrefKeyTestsApp.swift
+//  PrefKeyTests
+//
+//  Created by Miroslav Taleiko on 25.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct PrefKeyTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
